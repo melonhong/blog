cb9a6f7c062af451f480821fee1be596027142de
melonhong <melonhong46@gmail.com>
2025-11-11 03:10:52 +0900

    feat: 뮤직 플레이어 js 코드 분리
    - `applemusic.html`에 존재하던 js 코드를 가져옴
    - 요소가 존재하지 않으면 코드가 실행되지 않도록 개선

---
+++ _javascript/modules/components/music.js
@@ -0,0 +1,36 @@
+const musicbtn = document.getElementById('music-btn');
+const musicPlayer = document.getElementById('music-player');
+const overlay = document.getElementById('overlay');
+
+export function showPlayer() {
+  if (musicPlayer === null || overlay === null) return;
+
+  musicPlayer.classList.remove('music-player-hidden');
+  musicPlayer.classList.add('music-player-popup');
+  musicPlayer.style.animation = 'fadeIn 1s forwards';
+  overlay.classList.remove('music-player-hidden');
+}
+
+export function removePlayer() {
+  if (musicPlayer === null || overlay === null) return;
+
+  musicPlayer.classList.add('music-player-hidden');
+  overlay.classList.add('music-player-hidden');
+}
+
+if (musicbtn) {
+  musicbtn.addEventListener('mouseenter', showPlayer);
+
+  musicbtn.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    showPlayer();
+  });
+}
+
+if (musicPlayer) {
+  musicPlayer.addEventListener('mouseleave', removePlayer);
+}
+
+if (overlay) {
+  overlay.addEventListener('click', removePlayer);
+}
